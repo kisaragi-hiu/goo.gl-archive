b25d93fa457fe5500f6bb0be4e7eb932e0c93f4b
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-08-02 03:15:36 +0900

    merge: try not using WAL
    See if this reduces the amount of writes that's needed.

--- merge.ts
+++ merge.ts
@@ -14,8 +14,12 @@ untrusted input into it.`);
 
 const db = new Database(firstFile, { create: false, strict: true });
 db.run(`attach '${secondFile}' as second;`);
-db.run(`BEGIN;`);
+db.run(`PRAGMA journal_mode=DELETE;`);
+console.log("Inserting mapping");
+// db.run(`BEGIN;`);
 db.run(`INSERT OR REPLACE INTO mapping SELECT * FROM second.mapping`);
+console.log("Inserting errors");
 db.run(`INSERT OR REPLACE INTO errors SELECT * FROM second.errors`);
-db.run(`COMMIT;`);
+// db.run(`COMMIT;`);
+console.log("Done");
 db.run(`detach second;`);
