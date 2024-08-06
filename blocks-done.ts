0570e22013fedd319bd7018e345d5e829126ccb4
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-08-06 10:03:29 +0900

    double checked up to V0000

--- blocks-done.ts
+++ blocks-done.ts
@@ -2,7 +2,7 @@ export default [
   { init: "0", until: "0000" },
   { init: "0000", until: "00000" },
   { init: "00000", until: "F0000" },
-  { init: "F0000", until: "J0000" },
+  { init: "F0000", until: "V0000" },
   { init: "a0000", until: "c0000" },
   { init: "0", until: "0000", prefix: "fb" },
   { init: "0000", until: "00000", prefix: "fb" },
