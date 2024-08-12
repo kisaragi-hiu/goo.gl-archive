95a102c944621f7607692be86172282fdfa528f2
Kisaragi Hiu <mail@kisaragi-hiu.com>
2024-08-13 04:15:08 +0900

    merge.ts: make the interface easier to use

--- merge.ts
+++ merge.ts
@@ -1,25 +1,52 @@
 import { Database } from "bun:sqlite";
+import { parseArgs } from "node:util";
+import { existsSync } from "node:fs";
 
-const firstFile = Bun.argv[2];
-const secondFile = Bun.argv[3];
-
-if (firstFile === "--help" || firstFile == "-h") {
-  console.log(`${Bun.argv[1]} <first> <second>
-
-Merge rows from <second> into <first>.
-
-The <second> argument is replaced straight into a query, so do not pass
-untrusted input into it.`);
+function insertDbInto({
+  target,
+  source,
+}: {
+  target: Database;
+  source: string;
+}) {
+  target.run(`attach ? as second;`, [source]);
+  target.run(`PRAGMA journal_mode=DELETE;`);
+  console.log(`Inserting ${source}`);
+  console.log("  mapping...");
+  // target.run(`BEGIN;`);
+  target.run(`INSERT OR REPLACE INTO mapping SELECT * FROM second.mapping`);
+  console.log("  errors...");
+  target.run(`INSERT OR REPLACE INTO errors SELECT * FROM second.errors`);
+  // target.run(`COMMIT;`);
+  console.log("  done");
+  target.run(`detach second;`);
 }
 
-const db = new Database(firstFile, { create: false, strict: true });
-db.run(`attach '${secondFile}' as second;`);
-db.run(`PRAGMA journal_mode=DELETE;`);
-console.log("Inserting mapping");
-// db.run(`BEGIN;`);
-db.run(`INSERT OR REPLACE INTO mapping SELECT * FROM second.mapping`);
-console.log("Inserting errors");
-db.run(`INSERT OR REPLACE INTO errors SELECT * FROM second.errors`);
-// db.run(`COMMIT;`);
-console.log("Done");
-db.run(`detach second;`);
+async function main() {
+  const parsedArgs = parseArgs({
+    args: process.argv.slice(2),
+    allowPositionals: true,
+    options: {
+      db: { type: "string" },
+      help: { type: "boolean", short: "h" },
+    },
+  });
+  const dbFile = parsedArgs.values.db ?? "all.sqlite";
+  if (parsedArgs.values.help) {
+    console.log(`${process.argv[1]} FILES...
+
+Merge sqlite FILES into all.sqlite.
+
+Options:
+  --db <file>: write into this file instead of all.sqlite
+  --help: show help (this message)`);
+  } else {
+    const target = new Database(dbFile, { create: false, strict: true });
+    for (const file of parsedArgs.positionals) {
+      if (!existsSync(file)) throw new Error(`${file} does not exist!`);
+      insertDbInto({ target: target, source: file });
+    }
+  }
+}
+
+main();
